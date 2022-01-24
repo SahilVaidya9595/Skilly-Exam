e0f84d2fc92ec95cfe8876fc197a358be25636ef
HarshalSangme <harshalsangme10@gmail.com>
2022-01-24 16:38:23 +0530

    Student Authentication done

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const Student = require('../module/studentSchema');
+const bcrypt = require('bcrypt')
 
 
 const router = express.Router();
@@ -6,7 +8,6 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.send('hello world from server router js');
 });
-
 
 router.post('/register', async (req, res) => {
 
@@ -17,24 +18,30 @@ router.post('/register', async (req, res) => {
         return res.status(422).json('Plz filled the field properly');
     }
 
-    const student = require('../module/studentSchema');
-
     try {
 
-        const userExist = await student.findOne({email: email});
+        const userExist = await Student.findOne({email: email});
 
         if (userExist) {
             return res.status(422).json({error: 'Email already exist'});
         }
 
-        const student_new = new student({firstname, lastname, age, gender, email, phone_no, username, password });
+        const student_new = new Student({firstname, lastname, age, gender, email, phone_no, username, password });
+       
+        // Password Encryption Decription
 
+        const salt = await bcrypt.genSalt(10);
+        student_new.password = await bcrypt.hash(student_new.password, salt);
+
+
+        // Saving New Studnet Data in MongoDb Atlas
         const studentRegistered = await student_new.save();
 
         if (studentRegistered)
         {
             res.status(201).json('Student Registered Succesfully');
         } 
+
 
     } catch (err) {
         console.log(err);
@@ -46,8 +53,53 @@ router.post('/register', async (req, res) => {
     // res.send('mera register page');
 });
 
-router.post('/signin', (req, res) => {
-    console.log(req.body);
+
+// login route
+
+router.post('/signin', async (req, res) => {
+
+    try {
+        const {email, password} = req.body; 
+        
+        if (!email || !password)
+        {
+            return res.status(400).json('Plz filled the field properly');
+        }
+
+        const studentLogin = await Student.findOne({email:email});
+
+        if (studentLogin)
+        {
+            const isMatch = await bcrypt.compare(password, studentLogin.password);
+
+            if (!isMatch)
+            {
+                res.json({error: "Student not found"});
+            }
+            else {
+                console.log(studentLogin);
+                res.json({message: "Student Logged in successully"});
+            }
+        } else {
+            res.json({error: "Student not found"});
+        }
+
+        const isMatch = await bcrypt.compare(password, studentLogin.password);
+
+        if (!isMatch)
+        {
+            res.json({error: "Student not found"});
+        }
+
+        console.log(studentLogin);
+        res.json({message: "Student Logged in successully"});
+
+    } catch(err) {
+        console.log(err);
+    }
+
+    // console.log(req.body);
+
 });
 
 
